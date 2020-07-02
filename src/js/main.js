45211a4d200cfddcc01565048a07258fd4f0d62f
Mariya Lubashevskaya <Selestina84@gmail.com>
2020-07-02 23:41:42 +0800

    add showMoreStyles

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import modals from './modules/modals';
-import sliders from './modules/sliders'
+import sliders from './modules/sliders';
+import showMoreStyles from './modules/showMoreStyles';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict'
 
   modals();
   sliders('.feedback-slider-item','gorizontal','.main-prev-btn','.main-next-btn');
-  sliders('.main-slider-item','vertical')
+  sliders('.main-slider-item','vertical');
+  showMoreStyles('.button-styles','.styles-2')
 })
